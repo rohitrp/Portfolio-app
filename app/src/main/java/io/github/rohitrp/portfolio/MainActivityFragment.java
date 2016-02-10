08fc906ef5742b4aa34226a55533d9c318b22f06
Rohit Prasad <rohitrp3@gmail.com>
2016-02-10 22:32:07 +0530

    feat: Display toast or snackbar when a button is clicked
    For android versions higher than or equal to Lollipop, snackbar is displayed.   For other versions, a toast is displayed.

--- app/src/main/java/io/github/rohitrp/portfolio/MainActivityFragment.java
+++ app/src/main/java/io/github/rohitrp/portfolio/MainActivityFragment.java
@@ -1,12 +1,16 @@
 package io.github.rohitrp.portfolio;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,7 +29,7 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        String[] appsName = {
+        final String[] appsName = {
                 "Spotify Streamer",
                 "Scores App",
                 "Library App",
@@ -45,6 +49,18 @@ public class MainActivityFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.apps_listview);
         listView.setAdapter(appsAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String appName = appsName[position];
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    Toast.makeText(getContext(), appName, Toast.LENGTH_SHORT).show();
+                } else {
+                    Snackbar.make(view, appName, Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
         return rootView;
     }
 }
